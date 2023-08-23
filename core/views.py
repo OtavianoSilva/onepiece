3ebc36bb91a3cf845ffa5b6b6cfea80ee8d0d32d
Otaviano Silva <otavianosilva.escritor@gmail.com>
2023-08-23 10:24:30 -0300

    change background color

--- core/views.py
+++ core/views.py
@@ -10,13 +10,16 @@ class MangaTimeLineView(ListView):
 
     def get(self, request, slug=None, *args, **kwargs):
         if slug is None:
+            self.context["bodycolor"]= "#ffe797"
             self.context["media"] = Chapter.objects.all()
         else:
             if int(slug) in range(1997, 2024):
+                self.context["bodycolor"] = "#ff96bd"
                 self.context["media"] = Chapter.objects.filter(year=slug)
             else:
                 search = Chapter.objects.filter(arc__saga__id=slug)
                 if search.count() >= 1:
+                    self.context["bodycolor"] = "#cffff1"
                     self.context["media"] = search
                 else: return render(request, "404.html")
 
@@ -31,13 +34,16 @@ class AnimeTimeLineView(ListView):
 
     def get(self, request, slug=None, *args, **kwargs):
         if slug is None:
+            self.context["bodycolor"]= "#ffe797"
             self.context["media"] = Episode.objects.all()
         else:
             if int(slug) in range(1999, 2024):
+                self.context["bodycolor"] = "#ff96bd"
                 self.context["media"] = Episode.objects.filter(year=slug)
             else:
                 search = Episode.objects.filter(arc__saga__id=slug)
                 if search.count() >= 1:
+                    self.context["bodycolor"] = "#cffff1"
                     self.context["media"] = search
                 else: return render(request, "404.html")
 
